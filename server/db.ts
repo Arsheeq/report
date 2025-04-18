980724934f54ffd8f50f8cfb0b99879615f584e0
root <root@DESKTOP-VTNL0D0.>
2025-04-18 13:56:28 +0000

    python-testing

--- server/db.ts
+++ server/db.ts
@@ -3,7 +3,7 @@ const { Pool } = pkg;
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from '@shared/schema';
 
-const DATABASE_URL = 'postgresql://postgres:postgres@13.203.224.95:5432/nubinix_db';
+const DATABASE_URL = 'postgresql://postgres:postgres@13.203.194.88:5432/nubinix_db';
 
 export const pool = new Pool({
   connectionString: DATABASE_URL,
